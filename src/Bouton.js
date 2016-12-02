e3055a55a668afacb90e0055f2a7d1ad2326428d
alma31 <lenni31@neuf.fr>
2016-12-02 11:16:37 +0100

    ajout de bouton.css

--- src/Bouton.js
+++ src/Bouton.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './index.css';
+import './bouton.css';
 
 class Bouton extends Component {
 	render() {
 		return (
 			<div className="Bouton">
-     			<a href="tel:+33601648735"><button>appel</button></a>
+     			<a href="tel:+33601648735"><button id="BtnCall">appel</button></a>
      		</div>
      	);
 	}
