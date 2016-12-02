cf014bf6a313105412e49165c3bde61430eac650
Lilia <geertszelia@gmail.com>
2016-12-02 12:11:53 +0100

    ajout du bouton et select modifié

--- src/Bouton.js
+++ src/Bouton.js
@@ -5,7 +5,7 @@ class Bouton extends Component {
 	render() {
 		return (
 			<div className="Bouton">
-     			<a href="tel:+33601648735"><button id="BtnCall">Appeler[nomEntreprise]</button></a>
+     			<a href="tel:+33601648735"><button id="BtnCall"><i className="call icon"></i>[nomEntreprise]</button></a>
      		</div>
      	);
 	}
