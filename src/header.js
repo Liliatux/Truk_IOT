68ac5e1e3a344dacd0399a97e878ecd3cefe3d79
Fred <delon.frederic.pro.job@gmail.com>
2016-12-02 14:25:51 +0100

    add img + menu headerv1

---
+++ src/header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+
+	render(){
+		return (
+			<div className="menu">
+			<h1 className="ui header">
+			<img id="imgMenu" className="ui image" src="map2menu.png"/>
+			<div id="title" className="content">Where i go ? </div>
+			</h1>
+			</div>
+
+			)
+	}
+
+}
+
+export default Header;
