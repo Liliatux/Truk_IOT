5c3a4c2d148604e948edfd19d4afd7926d01c0f9
Lilia <geertszelia@gmail.com>
2016-12-02 14:50:10 +0100

    Ajout de la zone pour implementer le svg

--- src/Svg.js
+++ src/Svg.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './svg.css';
 
 class Svg extends Component {
 	render() {
 		return (
-			<svg className="svg" xmlns="">
-
-			</svg>
+			<div className="Svg">
+				<svg width="100%" height="100vh" xmlns="http://www.w3.org/2000/svg">
+				    <line className="Line" x1="2.5%" y1="0.1%" x2="95%" y2="0.1%"/>
+				    <line className="Line" x1="2.5%" y1="0.1%" x2="2.5%" y2="95%"/>
+				</svg>
+			</div>
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Select from './Select';
+import Svg from './Svg';
 import Bouton from './Bouton';
 import './index.css';
 
 ReactDOM.render(
 	<div>
 	<Select />
+	<Svg />
 	<Bouton />
 	</div>
 	, document.getElementById('root')
