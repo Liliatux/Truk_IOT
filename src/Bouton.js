6723cca7554bb7f3626a3f246ed873a65da35438
Fred <delon.frederic.pro.job@gmail.com>
2016-12-02 11:33:05 +0100

    semantic selecteur + fonction btn test

--- src/Bouton.js
+++ src/Bouton.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+
+
 class Bouton extends Component {
+
+	
 	render() {
 		return (
 			<div className="Bouton">
-     			<a href="tel:+33601648735"><button>appel</button></a>
-     		</div>
-     	);
+			<a href="tel:0000000000"><button>appel</button></a>
+			</div>
+			);
 	}
 }
 
