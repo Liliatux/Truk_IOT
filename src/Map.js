e5c7b7607a4b265f188b5dee076350a5fc410e79
Lilia <geertszelia@gmail.com>
2016-12-02 09:19:38 +0100

    Ajout de l'image map

---
+++ src/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+const src1 = '/Image/map';
+
+const ImageExampleCentered = () => (
+	<Image src={src1} size='medium' centered />
+);
+
+export default ImageExampleCentered;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Select from './Select';
 import Bouton from './Bouton';
+import Map from './Map';
 import './index.css';
 
 ReactDOM.render(<Select />,
   document.getElementById('root')
+);
+
+ReactDOM.render(<Map />,
+  document.getElementById('image')
 );
 
 ReactDOM.render(<Bouton />,
