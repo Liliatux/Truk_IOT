f56d35e4781a4deae203cc7f511078a897e059c7
Lilia <geertszelia@gmail.com>
2016-12-02 09:37:13 +0100

    supression du map

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,3 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
 
-const src1 = '/Image/map';
-
-const ImageExampleCentered = () => (
-	<Image src={src1} size='medium' centered />
-);
-
-export default ImageExampleCentered;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Select from './Select';
 import Bouton from './Bouton';
-import Map from './Map';
 import './index.css';
 
 ReactDOM.render(<Select />,
   document.getElementById('root')
-);
-
-ReactDOM.render(<Map />,
-  document.getElementById('image')
 );
 
 ReactDOM.render(<Bouton />,
