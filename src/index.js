456ba9abf67ace7302fa00491249099705dc0095
Fred <delon.frederic.pro.job@gmail.com>
2016-12-02 12:00:41 +0100

    modif gitignore /  script index et css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import Select from './Select';
 import Bouton from './Bouton';
 import './index.css';
+import './Select.css';
 
-ReactDOM.render(<Select />,
-  document.getElementById('root')
-);
-
-ReactDOM.render(<Bouton />,
-  document.getElementById('bouton')
+ReactDOM.render(
+	<div>
+	<Select />
+	<Bouton />
+	</div>
+,document.getElementById('root')
 );
