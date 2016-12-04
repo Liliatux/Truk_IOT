2b09e8affb42ae152f8a307705f5d5ad8cdeb26e
alma31 <lenni31@neuf.fr>
2016-12-04 15:44:26 +0100

    menu responsive ok lenni

--- src/header.js
+++ src/header.js
@@ -7,9 +7,6 @@ class Header extends Component {
 		return (
 			<div className="menu">
 				<img id="imgMenu" className="ui image" src="logo.png" alt="menu clickable"/>
-				<h1 className="ui header">
-					<div id="title" className="content">Where i go ? </div>
-				</h1>
 			</div>
 
 		);
--- src/menu.js
+++ src/menu.js
@@ -6,7 +6,7 @@ import './menu.css';
 class Menu extends Component {
 	render() {
 		return (
-			<div><i id="menu" className="list layout large icon"></i></div>
+			<div><i id="menu" className="list Content large icon"></i></div>
      	);
 	}
 }
