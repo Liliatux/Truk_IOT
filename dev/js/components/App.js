bc5f602c3af57b3dfe44188abe1128d01800c5ce
Lilia <geertszelia@gmail.com>
2016-12-04 18:12:28 +0100

    Mise en forme pour la demo

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../containers/Header';
-import Menu from '../containers/Menu';
 import Select from '../containers/Select';
 import Map from '../containers/Map';
 import Bouton from '../containers/Bouton';
@@ -8,7 +7,6 @@ import Bouton from '../containers/Bouton';
 const App = () => (
 	<div>
 		<Header/>
-		<Menu/>
 		<Select/>
 		<Map/>
 		<Bouton/>
