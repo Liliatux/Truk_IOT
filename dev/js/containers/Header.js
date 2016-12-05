3e4d3dc9aedbedbf5fd27f41dc89af9b4c4a1861
Lilia <geertszelia@gmail.com>
2016-12-05 15:45:18 +0100

    Modif de l'indentation

--- dev/js/containers/Header.js
+++ dev/js/containers/Header.js
@@ -6,15 +6,14 @@ class Header extends Component {
 	render(){
 		return (
 			<div>
-			<div className="menu">
-			<div onClick={this.onItemClick}><a href="#"><i id="menu" className="list Content large icon"></i></a></div>
-			<img id="imgMenu" className="ui image" src="../image/logo.png" alt="menu clickable"/>
+				<div className="menu">
+					<div onClick={this.onItemClick}><a href="#"><i id="menu" className="list Content large icon"></i></a></div>
+					<img id="imgMenu" className="ui image" src="../image/logo.png" alt="menu clickable"/>
+				</div>
+				<div className="slide">
+					<h1>teste</h1>
+				</div>
 			</div>
-			<div className="slide">
-			<h1>teste</h1>
-			</div>
-			</div>
-
 			);
 	}
 
