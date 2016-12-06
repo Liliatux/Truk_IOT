8f5e1a0389f8bb1f04f6ed78b4b6d8567d85be61
Fred <delon.frederic.pro.job@gmail.com>
2016-12-06 13:37:09 +0100

    infos json update + modif boutton et select

--- dev/js/containers/Select.js
+++ dev/js/containers/Select.js
@@ -22,29 +22,29 @@ class Select extends Component {
   render() {
     let items = this.state.items.map(item => {
       let tab = JSON.stringify([item.Phone,item.Entreprise]);
-      return <option id="option" onChange={this.update.bind(this)} className="item" value={tab} key={item.Entreprise}>{item.Entreprise}</option>
+      return <option  id="option" onChange={this.update.bind(this)} className="item" value={tab} key={item.Entreprise}><img src={item.Logo} alt="logo" className="img-select"/>{item.Entreprise}</option>
     });
     
     return(
       <div className="Select">
         <select id="selectTel" onChange={this.update.bind(this)} >
-        {items}
+          {items}
         </select>
       </div>
-      );
+        );
+    }
   }
-}
 
 
-function matchStateToProps(state){
-  return {
+  function matchStateToProps(state){
+    return {
       global: state.global
+    }
   }
-}
 
-function matchDispatchToProps(dispatch){
-  return bindActionCreators({ChangeStatus : ChangeStatus}, dispatch);
-}
+  function matchDispatchToProps(dispatch){
+    return bindActionCreators({ChangeStatus : ChangeStatus}, dispatch);
+  }
 
-export default connect(matchStateToProps, matchDispatchToProps)(Select);
+  export default connect(matchStateToProps, matchDispatchToProps)(Select);
 
