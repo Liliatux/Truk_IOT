9f45c154a37491c572006a0328bac99a35c8581e
Lilia <geertszelia@gmail.com>
2016-12-06 13:34:59 +0100

    Modif couleur select + responsive

--- dev/js/containers/Select.js
+++ dev/js/containers/Select.js
@@ -22,16 +22,16 @@ class Select extends Component {
   render() {
     let items = this.state.items.map(item => {
       let tab = JSON.stringify([item.Phone,item.Entreprise]);
-      return <option id="option" onChange={this.update.bind(this)} className="item" value={tab} key={item.Entreprise}>{item.Entreprise}</option>
+      return <option onChange={this.update.bind(this)} className="item" value={tab} key={item.Entreprise}>{item.Entreprise}</option>
     });
     
     return(
       <div className="Select">
-        <select id="selectTel" onChange={this.update.bind(this)} >
+        <select className="selectTel" onChange={this.update.bind(this)} >
         {items}
         </select>
       </div>
-      );
+    );
   }
 }
 
