e9fe9a6acb11a784a8b131758eea732685de4339
jordan <jordan.lefort@laposte.net>
2016-12-09 21:44:42 +0100

    test pwa

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('pwa').then(cache => {
+      return cache.addAll([
+        '/',
+        '/sw.js',
+        '/index.html',
+        '/bundle.min.js',
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
