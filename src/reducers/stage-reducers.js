f9752085011e6219350c4d8ceff65fb8534e88f2
Lilia <geertszelia@gmail.com>
2016-12-13 13:45:06 +0100

    Bouton appel en position fixed

--- src/reducers/stage-reducers.js
+++ src/reducers/stage-reducers.js
@@ -1,14 +1,15 @@
 export default function(state={
-	default:false,
+	depart:false,
 	name:'Rez-de-Chaussée',
 	icon:'arrow up inverted large icon',
 },action){
 	switch(action.type){
 		case "UPDATE-STAGE":
 			return{
-				state, default : action.value, name: action.value? '1er Etage':'Rez-de-Chaussée', icon: action.value? 'arrow down inverted large icon':'arrow up inverted large icon'
+				state, depart : action.value, name: action.value? '1er Etage':'Rez-de-Chaussée', icon: action.value? 'arrow down inverted large icon':'arrow up inverted large icon'
 			}
-			break;
+			break
+			default: return state;
 	}
 	return state;
 }
