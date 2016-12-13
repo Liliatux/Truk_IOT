cb917fa6e268838989b4319b4ddb7e2b433ba5fa
jordan <jordan.lefort@laposte.net>
2016-12-13 16:00:40 +0100

    add connected camp path

--- src/containers/donnees.js
+++ src/containers/donnees.js
@@ -20,7 +20,7 @@ var donnees =
     {
       "Entreprise": "Connected Camp",
       "Phone": "NC",
-      "rdc" : "<path className='target' d='M900 650l-3 3 3 3-7 8h-7l5.5 5.5-8.5 11.269v1.231h1.231l11.269-8.5 5.5 5.5v-7l8-7 3 3 3-3-15-15zM14'></path>",
+      "rdc" : "<path d='M600,572 L810,572  ' fill='none' className='animate' stroke='red' strokeWidth='5'></path><path className='target' d='M900 650l-3 3 3 3-7 8h-7l5.5 5.5-8.5 11.269v1.231h1.231l11.269-8.5 5.5 5.5v-7l8-7 3 3 3-3-15-15zM14'></path>",
       "floor":"none",
       "Web": "http://leconnected.camp/"
     },
