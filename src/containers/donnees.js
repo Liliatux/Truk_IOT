bbe7a1cd1ef7f22778a17fa0a1c8179ec2cc6002
Fred <delon.frederic.pro.job@gmail.com>
2016-12-13 16:54:19 +0100

    fly

--- src/containers/donnees.js
+++ src/containers/donnees.js
@@ -49,7 +49,7 @@ var donnees =
   "Entreprise": "Flylight",
   "Phone": "06 09 05 76 96",
   "rdc" : "",
-  "floor":"",
+  "floor":"<path className='target' d='M830 550l-3 3 3 3-7 8h-7l5.5 5.5-8.5 11.269v1.231h1.231l11.269-8.5 5.5 5.5v-7l8-7 3 3 3-3-15-15zM14'></path>",
   "Web": ""
 },
 
